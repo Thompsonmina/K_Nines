a6b472fed1a0a0516ede51fa874ecbc7623f32e7
Bolarinwa Owuogba <bhorlarinwah@gmail.com>
2022-05-03 10:00:45 +0100

    ch: remove unnecessary console.logs in frontend.

--- src/components/minter/nfts/index.js
+++ src/components/minter/nfts/index.js
@@ -19,126 +19,123 @@ import { Row, Button } from "react-bootstrap";
 import happy from "../../../assets/happy_puppy_barks.wav";
 import hunger from "../../../assets/hungry_puppy.wav";
 
-
 const NftList = ({ minterContract, name }) => {
-    const { performActions, address } = useContractKit();
-    const [nfts, setNfts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [units, setUnits] = useState({})
-    const [onlyMine, setOnlyMine] = useState(false)
+  const { performActions, address } = useContractKit();
+  const [nfts, setNfts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [units, setUnits] = useState({});
+  const [onlyMine, setOnlyMine] = useState(false);
 
-    
-
-    const getAssets = useCallback(async () => {
-        try {
-          setLoading(true);
-          const allNfts = await getNfts(minterContract);
-          if (!allNfts) return;
-          const units = await fetchUnitStates(minterContract)
-          setUnits(units)
-          setNfts(allNfts);
-        } catch (error) {
-          console.log({ error });
-        } finally {
-          setLoading(false);
-        }
-    }, [minterContract]);
-  
-    // const fetchStates = useCallback(async () => {
-      
-    //   if (minterContract) {
-    //     console.log("here")
-    //     const states = 
-    //     console.log(states)
-    //     return states
-    //   }
-    // }, [minterContract])
-
-    const addNft = async (data) => {
-        try {
-            setLoading(true);
-            await createNft(minterContract, performActions, data);
-            toast(<NotificationSuccess text="Updating K_nines...." />);
-            getAssets();
-        } catch (error) {
-            console.log({ error });
-            toast(<NotificationError text="Failed to create K_nine. Try Again" />);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const petK9 = async (index) => {
-      try {
-            setLoading(true);
-            await pet_k_nine(minterContract, performActions, index);
-            toast(<NotificationSuccess text="Your K_nine is happy...." />);
-            console.log(happy)
-            new Audio(happy).play();
-            getAssets();
-        } catch (error) {
-          console.log({ error });
-          toast(<NotificationError text="Failed to Pet, try again" />);
-      } finally {
-          setLoading(false);
-     }
-    }
-  
-    const feedK9 = async index => {
-      try {
-            setLoading(true);
-            await feed_k_nine(minterContract, performActions, index);
-            toast(<NotificationSuccess text="Your K_nine is fed...." />);
-            new Audio(hunger).play();
-            getAssets();
-        } catch (error) {
-          console.log({ error });
-          toast(<NotificationError text="Failed to feed k_nine, try again" />);
-      } finally {
-          setLoading(false);
-     }
-    }
-  
-  const reviveK9 = async index =>{
+  const getAssets = useCallback(async () => {
     try {
-            setLoading(true);
-            await revive_k_nine(minterContract, performActions, index);
-            toast(<NotificationSuccess text="K_nine successfully revived be better " />);
-            getAssets();
-        } catch (error) {
-          console.log({ error });
-          toast(<NotificationError text="Failed to revive K9, try again" />);
-      } finally {
-          setLoading(false);
-      }
+      setLoading(true);
+      const allNfts = await getNfts(minterContract);
+      if (!allNfts) return;
+      const units = await fetchUnitStates(minterContract);
+      setUnits(units);
+      setNfts(allNfts);
+    } catch (error) {
+      console.log({ error });
+    } finally {
+      setLoading(false);
     }
-      
-      useEffect(() => {
-        try {
-          if (address && minterContract) {
-            getAssets();
-          }
-        } catch (error) {
-          console.log({ error });
-        }
-      }, [minterContract, address, getAssets]);
-  
+  }, [minterContract]);
 
-      
-      if (address) {
-        return (
+  const addNft = async (data) => {
+    try {
+      setLoading(true);
+      await createNft(minterContract, performActions, data);
+      toast(<NotificationSuccess text="Updating K_nines...." />);
+      getAssets();
+    } catch (error) {
+      console.log({ error });
+      toast(<NotificationError text="Failed to create K_nine. Try Again" />);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const petK9 = async (index) => {
+    try {
+      setLoading(true);
+      await pet_k_nine(minterContract, performActions, index);
+      toast(<NotificationSuccess text="Your K_nine is happy...." />);
+
+      new Audio(happy).play();
+      getAssets();
+    } catch (error) {
+      console.log({ error });
+      toast(<NotificationError text="Failed to Pet, try again" />);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const feedK9 = async (index) => {
+    try {
+      setLoading(true);
+      await feed_k_nine(minterContract, performActions, index);
+      toast(<NotificationSuccess text="Your K_nine is fed...." />);
+      new Audio(hunger).play();
+      getAssets();
+    } catch (error) {
+      console.log({ error });
+      toast(<NotificationError text="Failed to feed k_nine, try again" />);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const reviveK9 = async (index) => {
+    try {
+      setLoading(true);
+      await revive_k_nine(minterContract, performActions, index);
+      toast(
+        <NotificationSuccess text="K_nine successfully revived be better " />
+      );
+      getAssets();
+    } catch (error) {
+      console.log({ error });
+      toast(<NotificationError text="Failed to revive K9, try again" />);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    try {
+      if (address && minterContract) {
+        getAssets();
+      }
+    } catch (error) {
+      console.log({ error });
+    }
+  }, [minterContract, address, getAssets]);
+
+  if (address) {
+    return (
+      <>
+        {!loading ? (
           <>
-            {!loading ? (
-              <>
-                <div className="d-flex justify-content-between align-items-center mb-4">
-                  <Button variant="link" onClick={() => {setOnlyMine(false)}}><h1 className="fs-4 fw-bold mb-0 text-dark">{name}</h1> </Button>
-                  <Button variant="link" onClick={() => setOnlyMine(true)}><h1 className="fs-4 fw-bold mb-0 text-dark">{"My K_Nines 🐕"}</h1> </Button>
-                  {
-                    <AddNfts save={addNft} address={address} />
-                  }
-                </div>
-                <Row xs={1} sm={2} lg={3} className="g-3  mb-5 g-xl-4 g-xxl-5">
-                  { ! onlyMine ? nfts.map((_nft) => (
+            <div className="d-flex justify-content-between align-items-center mb-4">
+              <Button
+                variant="link"
+                onClick={() => {
+                  setOnlyMine(false);
+                }}
+              >
+                <h1 className="fs-4 fw-bold mb-0 text-dark">{name}</h1>{" "}
+              </Button>
+              <Button variant="link" onClick={() => setOnlyMine(true)}>
+                <h1 className="fs-4 fw-bold mb-0 text-dark">
+                  {"My K_Nines 🐕"}
+                </h1>{" "}
+              </Button>
+              {<AddNfts save={addNft} address={address} />}
+            </div>
+            <Row xs={1} sm={2} lg={3} className="g-3  mb-5 g-xl-4 g-xxl-5">
+              {!onlyMine
+                ? nfts.map((_nft) => (
                     <Nft
                       key={_nft.index}
                       nft={{
@@ -149,43 +146,41 @@ const NftList = ({ minterContract, name }) => {
                       units={units}
                       revive={reviveK9}
                       client_address={address}
-                    />
-                  )) :
-                  nfts.filter(_nft => _nft.owner === address ).map((_nft) => (
-                    <Nft
-                      key={_nft.index}
-                      nft={{
-                        ..._nft,
-                      }}
-                      pet={petK9}
-                      feed={feedK9}
-                      units={units}
-                      revive={reviveK9}
-                      client_address={address}
-
                     />
                   ))
-                  }
-                  {console.log(onlyMine)}
-                </Row>
-              </>
-            ) : (
-              <Loader />
-            )}
+                : nfts
+                    .filter((_nft) => _nft.owner === address)
+                    .map((_nft) => (
+                      <Nft
+                        key={_nft.index}
+                        nft={{
+                          ..._nft,
+                        }}
+                        pet={petK9}
+                        feed={feedK9}
+                        units={units}
+                        revive={reviveK9}
+                        client_address={address}
+                      />
+                    ))}
+            </Row>
           </>
-        );
-      }
-      return null;
-    
-}
+        ) : (
+          <Loader />
+        )}
+      </>
+    );
+  }
+  return null;
+};
 
 NftList.propTypes = {
-    minterContract: PropTypes.instanceOf(Object),
-    updateBalance: PropTypes.func.isRequired,
-  };
-  
-NftList.defaultProps = {
-    minterContract: null,
+  minterContract: PropTypes.instanceOf(Object),
+  updateBalance: PropTypes.func.isRequired,
 };
-  
+
+NftList.defaultProps = {
+  minterContract: null,
+};
+
 export default NftList;
